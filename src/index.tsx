ee5e457cd144bfe0e38e417902212086310cea4f
sivanandanr <ranjith.sivanandan@redi.com>
2018-04-26 15:55:31 -0400

    base packaging setup and sample components

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import $ from "jquery";
+
 const jQuery = require('jquery');
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Layout } from './components/orderLayout/layout';
-
+//alert($("#hhead").text());
 
 const orderbook = document.getElementById('orderbook');
 render(<Layout/>, orderbook);
